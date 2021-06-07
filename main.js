9aefde6796c1e32094b5123dd543db7bff98477f
Kopamed <kopamedve@gmail.com>
2021-06-07 23:16:31 +0200

    propah skid m8

---
+++ main.js
@@ -0,0 +1,5 @@
+var placeholder = "Why are you looking at this bruh dont look at this no no no no stop";
+var imbored = "Bruh no srsly stop, this aint holsum 100";
+console.log(placeholder);
+console.log(imbored);
+console.log("yo now that ur here go check out this mf legend");
